0dd4440ac1bdbac695feb15c01f5d0515192d559
Pratistha-Chauhan <chuahanpratistha255@gmail.com>
2026-04-21 22:47:36 +0530

    Day 01:variable

---
+++ Day 01/datatype.py
@@ -0,0 +1,25 @@
+# Python supports several built-in data types:
+#     Integers (int): Whole numbers (e.g., 10, -5).
+#     Floats (float): Decimal numbers (e.g., 3.14, -0.001).
+#     Strings (str): Text data enclosed in quotes (e.g., "Hello", 'Python').
+#     Booleans (bool): Represents True or False.
+#     Lists: Ordered, mutable collections (e.g., [1, 2, 3]).
+#     Tuples: Ordered, immutable collections (e.g., (1, 2, 3)).
+#     Sets: Unordered collections of unique elements (e.g., {1, 2, 3}).
+#     Dictionaries: Key-value pairs (e.g., {"name": "Alice", "age": 25}).
+
+Name = "pratistha"
+print("Name:",Name)
+print("datatype is",type(Name))
+
+Age = 23
+print("Age:",Age)
+print("datatype is:",type(Age))
+
+Cgpa = 6.67
+print("Cgpa:",Cgpa)
+print("datatype is:",type(Cgpa))
+
+Is_completed = True
+print("Is_completed :",Is_completed)
+print("datatype is:",type(Is_completed))
---
+++ Day 01/typescasting.py
@@ -0,0 +1,35 @@
+#TypeCasting is the process of converting one datatype to another.
+#python provide the built-in function for typecasting.
+ 
+#int(): convert into integer
+#string(): convert into string
+#float(): convert into float
+#bool(): convert into boolean
+
+a = 34 
+b = "34"
+d = 223
+
+
+print(a)
+print(type(a))
+
+print(b)
+print(type(b))
+
+# Convert b to an integer
+c = int(b)
+print(c)
+print(type(c))
+
+e = str(d)
+print(e)
+print(type(e))
+
+f = bool(e)
+print(f)
+print(type(f))
+
+g = float(d)
+print(g)
+print(type(g))
