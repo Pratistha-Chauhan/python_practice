6429c0994671871fd6fc0b51bbeb5ff695837229
Pratistha-Chauhan <chuahanpratistha255@gmail.com>
2026-04-23 19:51:14 +0530

    Day 01 : Identity operators

---
+++ Day 01/Comparison operator.py
@@ -0,0 +1,11 @@
+# Comparison operators are used to compare two values:
+
+x = 5
+y = 3
+
+print(x == y) #  ==	Equal	x == y	
+print(x != y) #  !=	Not equal	x != y	
+print(x > y) #  >	Greater than	x > y	
+print(x < y) #  <	Less than	x < y	
+print(x >= y) #  >=	Greater than or equal to	x >= y	
+print(x <= y) #  <=	Less than or equal to	x <= y
