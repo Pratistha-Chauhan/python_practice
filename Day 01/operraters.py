30ff7d297384c2bc0edec317ad89f635a2d11209
Pratistha-Chauhan <chuahanpratistha255@gmail.com>
2026-04-23 19:42:08 +0530

    Day 01 :

---
+++ Day 01/operraters.py
@@ -0,0 +1,31 @@
+#operators is refers to the symbol and keywords that used in programming to perform operations on data, variables, and values.
+
+#types of operators are:
+#Arithmetic operators
+#Assignment operators
+#Comparison operators
+#Logical operators
+#Identity operators
+#Membership operators
+#Bitwise operators
+
+#Arithmetic operators are used with numeric values to perform common mathematical operations:
+n1 = int(input("enter the n1:"))
+n2 = int(input("enter the n2:"))
+print("sum of n1 & n2: ", n1 + n2 ) #addition
+print("sum of n1 & n2: ", n1 - n2 ) #subraction
+print("sum of n1 & n2: ", n1 * n2 ) #multiplication
+print("sum of n1 & n2: ", n1 / n2 ) #division
+print("sum of n1 & n2: ", n1 // n2 ) #Floor division
+print("sum of n1 & n2: ", n1 ** n2 ) #exponentiation
+print("sum of n1 & n2: ", n1 % n2 ) #modulus
+
+#Logical operators are used to combine conditional statements:
+x = 5
+
+print("AND Logical operators : ",x > 0 and x < 10)
+print("OR Logical operators : ",x < 5 or x > 10)
+print("NOT Logical operators",not(x > 3 and x < 10))
+
+
+
