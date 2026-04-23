44d0080b2a439b6f7ebf3dada1e456c2311114aa
Pratistha-Chauhan <chuahanpratistha255@gmail.com>
2026-04-23 18:46:18 +0530

    Day 01: use input function.py

---
+++ Day 01/comment , escape statement and print statement.py
@@ -0,0 +1,22 @@
+# comments are used to explain the code and are ignored by the python interpreter.
+# single line comment start with "#".
+# multi-line comments are enclosed in between ''' or """.
+
+
+"""hey,how are is everthing all right ., hope so uyou would be fine ,
+well nice to meet i am glaad to see you here and we meet 
+after a long time cause last time we met at carnival in 2024 """
+
+#escape statement is used to include special character in strings.
+#some common escape statement are:
+#\n : new line
+#\\ : backspace
+#\t : tab
+#\" : double quote
+#\' : single quotD
+
+print("Hello,\n Doctor")
+print("Hello,\t Doctor")
+print("Hello,\\ Doctor")
+print("Hello,\" Doctor")
+print("Hello,\' Doctor")
---
+++ Day 01/use input function.py
@@ -0,0 +1,9 @@
+#Input function () is the function which is used to take input from the user.
+
+Name = input ("Enter the name: ")
+print("Name is : ",Name)
+print(type(Name))
+
+
+Age = int(input("enter the age : "))
+print(f"Hello {Name}, you are {Age} years old.")
