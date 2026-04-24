d35b3a65b81e1192dfa9a3621466597dcf1748a8
Pratistha-Chauhan <chuahanpratistha255@gmail.com>
2026-04-24 12:26:34 +0530

    Day 02

---
+++ Day 02/logical operator.py
@@ -0,0 +1,7 @@
+#Logical operators are used to combine conditional statements:
+print("")
+x = 5
+
+print("AND Logical operators : ",x > 0 and x < 10)
+print("OR Logical operators : ",x < 5 or x > 10)
+print("NOT Logical operators",not(x > 3 and x < 10))
