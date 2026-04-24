5b0df4ffe68b827a8eefbd71cfe92ba9662c56a9
Pratistha-Chauhan <chuahanpratistha255@gmail.com>
2026-04-24 12:28:59 +0530

    Day 02

---
+++ Day 02/Python Loops.py
@@ -0,0 +1,19 @@
+#Python Loops
+
+#Python has two primitive loop commands:
+#while loops
+#for loops
+
+#while loops : With the while loop we can execute a set of statements as long as a condition is true.
+
+#Print i as long as i is less than 6:
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+
+
+#break statement : With the break statement we can stop the loop even if the while condition is true.
+
+
